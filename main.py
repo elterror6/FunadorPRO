b4bfd4b8922fd55fc4fb0aa6b84d63cafa3a1c57
elterror6 <76056638+elterror6@users.noreply.github.com>
2023-05-01 16:19:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import os
 import discord
 from discord.ext import commands
 from keep_alive import keep_alive
+secret = os.environ['TOKEN']
+# 
+# Eventos del bot
+#
+@bot.event
+# Evento nada más iniciarse ...
+async def on_ready():
+  # Estatus y actividad del bot
+  activity = discord.Streaming(name="Viendote desde la webcam | $help ", url="https://www.twitch.tv")
+  await bot.change_presence(status= discord.Status.online, activity=activity)
+  # Imprimir un mensaje cuando se inicie
+  print('Tamos activos papi')
+keep_alive()
+bot.run(secret)
